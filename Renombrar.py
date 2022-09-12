157495ed3f3f6afa2c3301fea9ff5ccd2379f0c3
Jose89fcb <60312427+jose89fcb@users.noreply.github.com>
2022-09-13 00:16:21 +0200

    Update Renombrar.py

--- Renombrar.py
+++ Renombrar.py
@@ -3,6 +3,6 @@ os.chdir(r'DIRECTORIO DE LAS FOTOS')  #Directorio de las fotos ejemplo: C:\Users
 i=1
 for file in os.listdir():
      src=file
-     dst="Fotos"+str(i)+".png"  #El nombre "Fotos" lo puedes quitar y dejarlo en blanco
+     dst="Fotos"+str(i)+".png"  #El nombre "Fotos" lo puedes quitar y dejarlo en blanco // .png lo puedes dejar igual o añadir otras extensiones como por ejemplo: .mp3/.mp4/.ogg ...
      os.rename(src,dst)
      i+=1
